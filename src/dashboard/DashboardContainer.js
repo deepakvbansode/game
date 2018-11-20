19c19c05e8318b09f80249605254eea11cb0563f
Deepak Bansode <deepak.bansode@weboniselab.com>
2018-11-20 09:13:45 +0530

    Ajax call

--- src/dashboard/DashboardContainer.js
+++ src/dashboard/DashboardContainer.js
@@ -1,74 +1,90 @@
-import React, { Component } from 'react';
-import FilterComponent from './FilterComponent';
-import ListComponent from './ListComponent';
+import React, { Component } from "react";
+import FilterComponent from "./FilterComponent";
+import ListComponent from "./ListComponent";
 
 class DashboardContainer extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      game:'FB',
-      filter:'UPCOMING',
-      data: [
-        
-      ]
+      game: "FB",
+      filter: "UPCOMING",
+      data: []
     };
     this.changeGame = this.changeGame.bind(this);
   }
-  
-  changeGame(selectedGame){
-    let data = this.getGameData(selectedGame);
-    this.setState({
-      game: selectedGame,
-      data: data
-    });
+
+  changeGame(selectedGame) {
+    // let data = this.getGameData(selectedGame);
+    // this.setState({
+    //   game: selectedGame,
+    //   data: data
+    // });
+    //ajax call
+    let url = "http://demo8926664.mockable.io/games-fb";
+    if (selectedGame === "BB") url = "http://demo8926664.mockable.io/games-bb";
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(myJson => {
+        console.log(JSON.stringify(myJson));
+        this.setState({
+          game: selectedGame,
+          data: myJson
+        });
+      });
+    //set data
   }
-  getGameData(game){
-    if(game === 'FB'){
+  getGameData(game) {
+    if (game === "FB") {
       return [
         {
-          team1:'abc',
-          team2:'pqr',
-          score1:20,
-          score2:19
+          team1: "abc",
+          team2: "pqr",
+          score1: 20,
+          score2: 19
         },
         {
-          team1:'stu',
-          team2:'zyx',
-          score1:2,
-          score2:3
+          team1: "stu",
+          team2: "zyx",
+          score1: 2,
+          score2: 3
         }
-      ]
-    }
-    else if(game === 'BB'){
+      ];
+    } else if (game === "BB") {
       return [
         {
-          team1:'MUMBAI',
-          team2:'PUNE',
-          score1:20,
-          score2:19
+          team1: "MUMBAI",
+          team2: "PUNE",
+          score1: 20,
+          score2: 19
         },
         {
-          team1:'BENGLUR',
-          team2:'HYD',
-          score1:2,
-          score2:3
+          team1: "BENGLUR",
+          team2: "HYD",
+          score1: 2,
+          score2: 3
         }
-      ]
+      ];
     }
     return [
       {
         team1: "India",
         team2: "Pakistan",
-        score1:300,
-        score2:250
+        score1: 300,
+        score2: 250
       }
-    ]
+    ];
   }
   render() {
     return (
       <div>
-          <FilterComponent game={this.state.game} filter={this.state.filter} changeGame={this.changeGame}></FilterComponent>
-          <ListComponent matches={this.state.data} game={this.state.game}></ListComponent>
+        <FilterComponent
+          game={this.state.game}
+          filter={this.state.filter}
+          changeGame={this.changeGame}
+        />
+        <ListComponent matches={this.state.data} game={this.state.game} />
       </div>
     );
   }
